c2123f144526a80545ec5606a39425db471d3fad
abhinav mulloli <abhinavmulloli1@gmail.com>
2023-09-30 12:57:24 +0530

    fourth

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app=express()
 app.use(express.json())
 // integrate frontend with server
 const cors=require('cors')
-app.use(cors({origin:'http://localhost:4200'}))
+app.use(cors({origin:'https://visionary-cucurucho-aea2b5.netlify.app/'}))
 
 //import jwt
 
